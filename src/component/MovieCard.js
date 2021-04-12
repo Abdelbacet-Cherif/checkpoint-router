d44374e4a31565d4e7c094921b92bb732871ea01
Abdelbacet <abdelbacetcherif@gmail.com>
2021-04-12 14:57:41 +0100

    adding router

---
+++ src/component/MovieCard.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import {Card, Button} from 'react-bootstrap'
+import ReactStars  from 'react-rating-stars-component';
+
+
+export default function MovieCard ({mov}) {
+    return (
+        <div >
+          {
+  <Card style={{ width: '18rem' }} > 
+  <Card.Img variant="top" src ={mov.images} className="img-movie" />
+  <Card.Body>
+    <Card.Title> 
+   {mov.titre} 
+    </Card.Title>
+    <Card.Text>
+   {mov.description}
+    </Card.Text> 
+    <div className = "star">
+    <ReactStars  
+    edit={false}
+                        name="rate"
+                        starCount={5}
+                        value={mov.rate}
+                        size={30}
+                    /> 
+        </div>
+  </Card.Body>
+</Card>}
+        </div>
+          )
+}
